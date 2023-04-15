6185f49fb0a799786fe96b5d1fd134641d056e89
kimagin <info@kimagination.net>
2023-04-15 13:59:04 +0200

    Creating the Navigation bar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    fontFamily: {
+      sans: ['Public Sans', 'sans-serif'],
+    },
+    fontWeight: {
+      light: 300,
+      base: 400,
+      bold: 700,
+    },
+    extend: {
+      fontSize: {
+        project: '18px',
+      },
+      colors: {
+        // Primary
+        'dark-blue': 'hsl(233, 26%, 24%)',
+        'lime-green': 'hsl(136, 65%, 51%)',
+        'bright-cyan': 'hsl(192, 70%, 51%)',
+
+        // Neutral
+        'grayish-blue': 'hsl(233, 8%, 62%)',
+        'light-grayish-blue': 'hsl(220, 16%, 96%)',
+        'very-light-gray': 'hsl(0, 0%, 98%)',
+        white: 'hsl(0, 0%, 100%)',
+      },
+    },
+  },
+  plugins: [],
 }
